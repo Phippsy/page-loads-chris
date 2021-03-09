62a01358610bfebbb7b2776c7b5ca6ad19239529
Donal <donal.phipps@gmail.com>
2021-03-09 20:40:48 +0000

    Initial commit. README + harfile-output.R

---
+++ README.Rmd
@@ -0,0 +1,68 @@
+---
+title: "Working HARder to fix page loads"
+author: "Donal Phipps"
+date: "09/03/2021"
+output: html_document
+---
+
+## An r-script to plot and simplify HAR file data
+
+This script reads in data from an HTTP ARchive format, pulls out some key data and creates a few simple plots to help understand those requests which appear to be occupying the most time during page load.
+
+It reads in the following variables from the .har file: 
+
+- url: the URL of the request
+- time: the time (in ms) taken to 
+- request_method: the type of HTTP request method (/verb)
+- response_size: the size, in bytes, of the given request
+- page: the page from which the request was initated
+- domain: the domain of the request. This is extracted by the script from the request url
+- mf_int: ("Mouseflow Interactive") - does this request appear to be made by Mouseflow in response to user interactions with the page (i.e. it may not represent the page weight, only the time and size used for Mouseflow to track user behaviour on the page). The assumption is made that any requests made to the domain mouseflow.com, followed by 'dom' or 'events' may be Mouseflow Interactive requests
+
+[Read more about generating .har files](https://confluence.atlassian.com/kb/generating-har-files-and-analyzing-web-requests-720420612.html)
+
+## Running the script
+
+### Install R
+
+Make sure that you have installed R on your machine. This can be downloaded from the Comprehensive R Archive Network (CRAN). [Access the download page](https://cran.r-project.org/mirrors.html).
+
+If you are using brew, it's nice and easy:
+
+```{console}
+brew install r
+```
+
+To check that R has been installed correctly, you can run the following at the terminal:
+
+```{console}
+which Rscript
+# /usr/local/bin/Rscript
+```
+
+## Run the script
+
+From the terminal, call the R interpreter (`Rscript`), followed by the script filename, followed by the path to the .har file which you would like to analyse.
+
+Example: 
+
+```{console}
+/usr/local/bin/Rscript harfile-output.R my_har_file.har 
+```
+
+Depending on what you see when typing `which Rscript`, you can typically call `/usr/local/bin/Rscript` if the call to `Rscript` causes any problems.
+
+You must only supply a single argument to a .har file after calling the R script. 
+
+## Output
+
+If successful, the script should create the following new files:
+
+1. "tabular_har_{pagename}.csv": a csv containing each of the variables which have been selected from the source .har file
+2. "scatter_{pagename}.png": a scatterplot which charts each request, showing response size on the X axis and download time on the Y axis. 
+3. "scatter_{pagename}_top20.png": a scatterplot based on #2, which has been filtered to show only the top 20 requests (theoretically, the "worst offenders") by response time.
+4. "requests_{pagename}.csv": a bar chart showing the aggregated time for requests, grouped by domain. This chart is intended to illustrate which domains are using the most request time during page download.
+
+## Issues
+
+In case of problems with the script, please contact me via Teams or email - I'm a bit rubbish at responding to github issues. 
---
+++ harfile-output.R
@@ -0,0 +1,103 @@
+if (!require(tidyverse)) install.packages("tidyverse")
+if (!require(HARtools)) install.packages("HARtools")
+if (!require(splashr)) install.packages("splashr")
+if (!require(ggrepel)) install.packages("ggrepel")
+if (!require(scales)) install.packages("scales")
+
+# Function: Read in har file,  return data frame --------------------------
+get_harspeed <- function(file) {
+  harfile <- readHAR(file)
+  
+  get_speeds <- function(li) {
+    url <- li$request$url
+    time <- li$time
+    request_method <- li$request$method
+    response_size <- li$response$content$size
+    data.frame(url = url,
+               time = time,
+               request_method = request_method,
+               response_size = response_size)
+  }
+  
+  # Convert nested/repeated harfile structure to data frame
+  res <- map_df(harfile$log$entries, get_speeds) %>% 
+    mutate(page = str_replace_all(.$url[1], ".*\\.com", ""))
+  
+  tidy <- res %>% 
+    mutate(domain = str_replace_all(url, ".*\\/\\/", "")) %>% 
+    mutate(domain = str_replace_all(domain, "\\/.*", ""),
+           mf_int = case_when(
+             str_detect(url, "mouseflow\\.com/(dom|events)") ~ "Mouseflow Interactive event",
+             # Assumption: requests to mouseflow.com with 
+             # dom/events query string are in response to user interaction, not part of page load
+             TRUE ~ "Non-interactive event"
+           ))
+  tidy
+}
+
+args_in <- commandArgs(trailingOnly = TRUE)
+file_safe <- stringr::str_replace_all(args_in, "^a-zA-Z0-9", "_")
+# print(length(args_in))
+# print(args_in)
+stopifnot("You must only supply one argument to this script" = length(args_in) == 1)
+
+har_df <- get_harspeed(args_in)
+  
+har_df %>% 
+    group_by(domain, mf_int) %>% 
+    summarise(time = sum(time)) %>% 
+    ungroup() %>% 
+    mutate(domain = fct_reorder(domain, time)) %>% 
+    ggplot(aes(domain, time, fill = mf_int)) + 
+    geom_col() + 
+    theme_bw() +
+    coord_flip() +
+    scale_y_continuous(labels = scales::comma) +
+    labs(y = "Time (ms)", x = "Request domain", title = paste0("Requests summary by domain"),
+         subtitle = paste0("Page: ", har_df$page[1]),
+         fill = "Script type (estimated)") +
+    theme(plot.title.position = "plot",
+          legend.position = "bottom",
+          legend.text = element_text(size = 5), legend.title = element_text(size = 6)) +
+  scale_fill_brewer()
+
+page_safe <- str_replace_all(har_df$page[1], "[^a-zA-Z0-9]", "_")
+
+ggsave(filename = paste0("requests_", page_safe, ".png"), width = 150, height = 180, units = "mm")
+
+top_df <- har_df %>% 
+  filter(!str_detect(mf_int, "Mouse")) %>% 
+  top_n(20, time) %>% 
+  mutate(item = case_when(
+    str_detect(url, "dtm") ~ "DTM",
+    str_detect(url, "/bundles") ~ str_replace_all(url, ".*bundles/|\\?.*", ""),
+    str_detect(url, "cdn\\.") ~ paste0("Asset ID: ", str_replace_all(url, ".*assetid=", "")),
+    str_detect(url, "oveo") ~ "CoveoHive",
+    TRUE ~ domain
+  ))
+
+interactive <- har_df %>% 
+  ggplot(aes(response_size, time)) +
+  geom_point() +
+  theme_bw() 
+
+interactive + geom_label_repel(data = top_df, aes(label = item)) +
+  labs(title = "Scatter of response size (x-axis) vs download time (y-axis), cytiva homepage",
+       x = "Response size (B)", y = "Time (ms)") +
+  theme_bw() + scale_x_continuous(labels = comma)
+
+ggsave(filename = paste0("scatter_", page_safe, ".png"), width = 450, height = 450, units = "mm")
+
+top_df %>% 
+  ggplot(aes(response_size, time)) +
+  geom_point() +
+  theme_bw() +
+  geom_label_repel(data = top_df, aes(label = item)) +
+  labs(title = "Scatter of response size (x-axis) vs download time (y-axis)\ntop 20 requests by response time",
+       subtitle = paste0("Page: ", har_df$page[1]),
+       x = "Response size (B)", y = "Time (ms)") +
+  theme_bw() + scale_x_continuous(labels = comma)
+
+ggsave(filename = paste0("scatter_", page_safe, "_top20.png"), width = 350, height = 350, units = "mm")
+write_excel_csv(har_df, paste0("tabular_har_", page_safe, ".csv"))
+ 
